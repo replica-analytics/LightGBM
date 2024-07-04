2abd7f06d10a7453b9dd409abc5dad30fdfb1ac8
DongchaoChen <dongchao.chen@gmail.com>
2024-07-04 14:56:26 -0400

    update setup.sh. test.sh and test 3.11

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+set -e -E -u -o pipefail
+
+# defaults
+AZURE=${AZURE:-"false"}
+IN_UBUNTU_BASE_CONTAINER=${IN_UBUNTU_BASE_CONTAINER:-"false"}
+SETUP_CONDA=${SETUP_CONDA:-"true"}
+
+ARCH=$(uname -m)
+
+
 if [[ $OS_NAME == "macos" ]]; then
     if  [[ $COMPILER == "clang" ]]; then
         brew install libomp
         if [[ $AZURE == "true" ]]; then
-            sudo xcode-select -s /Applications/Xcode_10.3.app/Contents/Developer || exit -1
+            sudo xcode-select -s /Applications/Xcode_13.1.0.app/Contents/Developer || exit 1
         fi
     else  # gcc
-        if [[ $TASK != "mpi" ]]; then
-            brew install gcc
-        fi
+        # Check https://github.com/actions/runner-images/tree/main/images/macos for available
+        # versions of Xcode
+        sudo xcode-select -s /Applications/Xcode_14.3.1.app/Contents/Developer || exit 1
+        brew install gcc
     fi
     if [[ $TASK == "mpi" ]]; then
         brew install open-mpi
@@ -17,12 +28,8 @@ if [[ $OS_NAME == "macos" ]]; then
     if [[ $TASK == "swig" ]]; then
         brew install swig
     fi
-    curl \
-        -sL \
-        -o miniforge.sh \
-        https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh
 else  # Linux
-    if [[ $IN_UBUNTU_LATEST_CONTAINER == "true" ]]; then
+    if [[ $IN_UBUNTU_BASE_CONTAINER == "true" ]]; then
         # fixes error "unable to initialize frontend: Dialog"
         # https://github.com/moby/moby/issues/27988#issuecomment-462809153
         echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
@@ -32,51 +39,83 @@ else  # Linux
             software-properties-common
 
         sudo apt-get install --no-install-recommends -y \
-            apt-utils \
             build-essential \
             ca-certificates \
             cmake \
             curl \
             git \
-            iputils-ping \
-            jq \
             libcurl4 \
-            libicu66 \
-            libssl1.1 \
-            libunwind8 \
+            libicu-dev \
+            libssl-dev \
             locales \
-            netcat \
-            unzip \
-            zip
+            locales-all || exit 1
         if [[ $COMPILER == "clang" ]]; then
             sudo apt-get install --no-install-recommends -y \
                 clang \
                 libomp-dev
+        elif [[ $COMPILER == "clang-17" ]]; then
+            sudo apt-get install --no-install-recommends -y \
+                wget
+            wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+            sudo apt-add-repository deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main
+            sudo apt-add-repository deb-src http://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main
+            sudo apt-get update
+            sudo apt-get install -y \
+                clang-17 \
+                libomp-17-dev
         fi
 
         export LANG="en_US.UTF-8"
+        sudo update-locale LANG=${LANG}
         export LC_ALL="${LANG}"
-        sudo locale-gen ${LANG}
-        sudo update-locale
+    fi
+    if [[ $TASK == "r-package" ]] && [[ $COMPILER == "clang" ]]; then
+        sudo apt-get install --no-install-recommends -y \
+            libomp-dev
     fi
     if [[ $TASK == "mpi" ]]; then
-        sudo apt-get update
-        sudo apt-get install --no-install-recommends -y \
-            libopenmpi-dev \
-            openmpi-bin
-    fi
-    if [[ $TASK == "gpu" ]]; then
-        sudo add-apt-repository ppa:mhier/libboost-latest -y
-        sudo apt-get update
-        sudo apt-get install --no-install-recommends -y \
-            libboost1.74-dev \
-            ocl-icd-opencl-dev
-        if [[ $IN_UBUNTU_LATEST_CONTAINER == "true" ]]; then
+        if [[ $IN_UBUNTU_BASE_CONTAINER == "true" ]]; then
+            sudo apt-get update
             sudo apt-get install --no-install-recommends -y \
-                pocl-opencl-icd
+                libopenmpi-dev \
+                openmpi-bin
+        else  # in manylinux image
+            sudo yum update -y
+            sudo yum install -y \
+                openmpi-devel \
+            || exit 1
         fi
     fi
-    if [[ $TASK == "cuda" || $TASK == "cuda_exp" ]]; then
+    if [[ $TASK == "gpu" ]]; then
+        if [[ $IN_UBUNTU_BASE_CONTAINER == "true" ]]; then
+            sudo apt-get update
+            sudo apt-get install --no-install-recommends -y \
+                libboost1.74-dev \
+                libboost-filesystem1.74-dev \
+                ocl-icd-opencl-dev
+        else  # in manylinux image
+            sudo yum update -y
+            sudo yum install -y \
+                boost-devel \
+                ocl-icd-devel \
+                opencl-headers \
+            || exit 1
+        fi
+    fi
+    if [[ $TASK == "gpu" || $TASK == "bdist" ]]; then
+        if [[ $IN_UBUNTU_BASE_CONTAINER == "true" ]]; then
+            sudo apt-get update
+            sudo apt-get install --no-install-recommends -y \
+                pocl-opencl-icd
+        elif [[ $(uname -m) == "x86_64" ]]; then
+            sudo yum update -y
+            sudo yum install -y \
+                ocl-icd-devel \
+                opencl-headers \
+            || exit 1
+        fi
+    fi
+    if [[ $TASK == "cuda" ]]; then
         echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
         apt-get update
         apt-get install --no-install-recommends -y \
@@ -94,17 +133,14 @@ else  # Linux
         apt-get install --no-install-recommends -y \
             cmake
     fi
-    if [[ $SETUP_CONDA != "false" ]]; then
-        ARCH=$(uname -m)
-        curl \
-            -sL \
-            -o miniforge.sh \
-            https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-${ARCH}.sh
-    fi
 fi
 
 if [[ "${TASK}" != "r-package" ]] && [[ "${TASK}" != "r-rchk" ]]; then
     if [[ $SETUP_CONDA != "false" ]]; then
+        curl \
+            -sL \
+            -o miniforge.sh \
+            https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-${ARCH}.sh
         sh miniforge.sh -b -p $CONDA
     fi
     conda config --set always_yes yes --set changeps1 no
